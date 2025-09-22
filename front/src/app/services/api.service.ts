d437fe8d7f5fc67aefec043661e49a8ca0036b28
Geanfranco <geanfranco1212@gmail.com>
2025-09-22 19:46:32 -0400

    Agrego frontend como carpeta normal

---
+++ front/src/app/services/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, provideEnvironmentInitializer } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environments'; 
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+  private base = length;
+
+  constructor() { }
+}
